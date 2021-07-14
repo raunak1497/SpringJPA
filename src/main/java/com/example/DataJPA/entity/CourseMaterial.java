0ecca3dc217b928424ec4dcb55112987214a8df3
Raunak Singh <raunak1497@gmail.com>
2021-07-14 15:52:45 +0530

    Added course and course material class

---
+++ src/main/java/com/example/DataJPA/entity/CourseMaterial.java
@@ -0,0 +1,29 @@
+package com.example.DataJPA.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseMaterial {
+
+    @Id
+    @SequenceGenerator(
+            name = "courseMaterial_sequence",
+            sequenceName = "courseMaterial_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "courseMaterial_sequence"
+    )
+    private Long courseMaterialId;
+    private String url;
+}
